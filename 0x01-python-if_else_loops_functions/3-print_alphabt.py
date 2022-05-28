059a15b25fa436dca5198084f486a46a3751a229
adeolaakee <adeolaakee@gmail.com>
2022-05-28 16:41:37 -0600

    complete if and elif in python part...

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for c in range(97, 123):
-    if c == 101 and c == 113:
+for char in range(97, 123):
+    if char == 101 and char == 113:
         continue
-    print("{:c}".format(c), end="")
+    print("{:c}".format(char), end="")
