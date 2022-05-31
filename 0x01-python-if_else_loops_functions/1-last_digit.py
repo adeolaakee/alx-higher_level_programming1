43d13c8834e2324454d53c0f9d83f3a91c9c42d5
adeolaakee <adeolaakee@gmail.com>
2022-05-31 15:18:40 +0000

    printing last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 import random
+
 number = random.randint(-10000, 10000)
 
-#To get the last digit of a num you you'll use modulus
-test =  number % 10
+# To get the last digit of a num you you'll use modulus
+test = number % 10
 
-#To print the last digit of negative number
-if (number < 0):
+# To print the last digit of negative number
+if number < 0:
     test = -number % 10
 
-if (test > 5):
+if test > 5:
     print(f"Last digit of {number:d} is {test:d} and is greater than 5")
-elif(test == 0):
+elif test == 0:
     print(f"Last digit of {number:d} is {test:d} and is 0")
-elif((test < 6) and (test != 0)):
+elif (test < 6) and (test != 0):
     print(f"Last digit of {number:d} is {test:d} and is less than 6 and not 0")
